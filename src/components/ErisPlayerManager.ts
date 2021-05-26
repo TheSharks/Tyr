68d88bea4e3c2fb4bcf148579e9721a63edc2046
Remco <hey@dougley.com>
2021-05-26 18:56:30 +0200

    heh

--- src/components/ErisPlayerManager.ts
+++ src/components/ErisPlayerManager.ts
@@ -73,7 +73,7 @@ export class ErisPlayerManager extends Collection<Player> {
       if (!this.#pending[data.guild_id]) {
         return
       }
-      player = new Player(this.selectBestLavalinkNode(data.endpoint), data.guild_id, data.shard)
+      player = new Player(this.selectBestLavalinkNode(data.endpoint), data.guild_id, data.shard.sendWS)
       this.set(data.guild_id, player)
     }
     player.once('ready', () => {
